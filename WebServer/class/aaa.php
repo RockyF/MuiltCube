cd61a86bb4d2138376e7a985bbbae5f7b42effd8
RockyF <rockyf@126.com>
2014-04-08 01:20:29 +0800

    commit at home

--- WebServer/class/aaa.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: RockyF
- * Date: 2014/4/8
- * Time: 1:17
- */ 
---
+++ WebServer/class/common.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: RockyF
+ * Date: 2014/4/8
+ * Time: 1:17
+ */
+
+class Common{
+	function __construct(){
+
+	}
+
+	function getColorList(){
+		return array("1"=>0xFFFFFF);
+	}
+}
